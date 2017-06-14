6332b14734152bb2fc1f342784479f099b4ed582
git-hub-1 <jobs.sanj@gmail.com>
2017-06-14 22:08:32 +0800

    test2

--- src/test/java/com/hello/maven/helloworldmaveneclipse/AppTest.java
+++ src/test/java/com/hello/maven/helloworldmaveneclipse/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
     {
        // assertTrue( true );
        // System.out.print(App.main(null));
-    	System.out.println("Inside AppTest.java " + App.testMaven("Hello World.14jun"));
+    	System.out.println("Inside AppTest.java " + App.testMaven("Hello World.14jun-10:08PM"));
     }
 
 
